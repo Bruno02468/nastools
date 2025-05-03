94917492f9f6b90711a489478c065d4bbd25b38b
Bruno Borges Paschoalinoto <bruno@paschoalinoto.com>
2025-05-03 20:23:36 -0300

    update deps, set window system on nastester

--- nastester/src/main.rs
+++ nastester/src/main.rs
@@ -1,6 +1,7 @@
 //! This is a stand-alone program to run and test MYSTRAN against other
 //! solvers, using their .F06 outputs and user-set criteria.
 
+#![windows_subsystem = "windows"]
 #![warn(missing_docs)] // almost sure this is default but whatever
 #![warn(clippy::missing_docs_in_private_items)] // sue me
 #![allow(clippy::needless_return)] // i'll never forgive rust for this
